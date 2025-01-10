ecadb25525f2f5dcb7e3365dd5bb0b1fc119bd1e
tealjapa <tealjapa@student.jyu.fi>
2025-01-10 09:11:28 +0200

    feat: separated processing and logic

--- huffman.py
+++ huffman.py
@@ -1,153 +1,53 @@
-#!/usr/bin/python
-import sys
-import math
-from functools import reduce
-
-CHARSET = " abcdefghijklmnopqrstuvwxyz"
 GRAPH = False
-USAGE = f"""
-usage:
-     dec <string to decode>
-     enc <string to encode>
+def build_codewords(p):
+    def codewords(p, t):
+        paths = []
+        for pi in p:
+            path = []
+            parent = pi
+            for i in range(len(t)):
+                if t[i][1] == parent:
+                    parent = t[i][0]
+                    path.append("1")
+                elif t[i][2] == parent:
+                    path.append("0")
+                    parent = t[i][0]
+            paths.append("".join(reversed(path)))
+        return paths
 
-Example:
-    >enc kissa istuu puussa
-     10100011001010101010110001001010111011010110101001100111010110101010101010110
+    def tree(t, i, ni):
+       if ni == 1:
+           return t
+       f, s = i[-ni], i[-(ni-1)]
+       n = f + s
+       t.append([n, s, f])
+       i.append(n)
+       if GRAPH:
+           print(f'{round(s,2)} -> {round(n,2)} [label="1"];')
+           print(f'{round(f,2)} -> {round(n,2)} [label="0"];')
+       return tree(t, sorted(i), ni-1)
+    return codewords(p, tree([], sorted(p), len(p)))
 
-    >dec 10100011001010101010110001001010111011010110101001100111010110101010101010110
-     kissa istuu puussa
-
-Please use only items from this charset:
-
-{CHARSET}
-
-I try to parse everything else away, but have not accounted for everything."""
-
-# Letter propabilities from
-# Lecture 4:
-#     Entropy and Data Compression (III): Shannon's Source Coding Theorem, Symbol Codes
-#     video: https://youtu.be/eHGqNvkL4n4?t=390
-LETTERS = {
-    " ":19.28,
-    "a":5.75,
-    "b":1.28,
-    "c":2.63,
-    "d":2.85,
-    "e":9.13,
-    "f":1.73,
-    "g":1.33,
-    "h":3.13,
-    "i":5.99,
-    "j":0.06,
-    "k":0.84,
-    "l":3.35,
-    "m":2.35,
-    "n":5.96,
-    "o":6.89,
-    "p":1.92,
-    "q":0.08,
-    "r":5.08,
-    "s":5.67,
-    "t":7.06,
-    "u":3.34,
-    "v":0.69,
-    "w":1.19,
-    "x":0.73,
-    "y":1.64,
-    "z":0.07
-}
-
-def codewords(p, t):
-    paths = []
-    for pi in p:
-        path = []
-        parent = pi
-        for i in range(len(t)):
-            if t[i][1] == parent:
-                parent = t[i][0]
-                path.append("1")
-            elif t[i][2] == parent:
-                path.append("0")
-                parent = t[i][0]
-        paths.append("".join(reversed(path)))
-    return paths
-
-def tree(t, i, ni):
-   if ni == 1:
-       return t
-   f, s = i[-ni], i[-(ni-1)]
-   n = f + s
-   t.append([n, s, f])
-   i.append(n)
-   if GRAPH:
-       print(f'{round(s,2)} -> {round(n,2)} [label="1"];')
-       print(f'{round(f,2)} -> {round(n,2)} [label="0"];')
-   return tree(t, sorted(i), ni-1)
-
-def encodings(p):
-   return codewords(p, tree([], sorted(p), len(p)))
-
-def encode(s, c):
+def encode(s, c, l):
    coded = []
    for letter in s:
-       encoding = c[list(LETTERS.keys()).index(letter)]
+       encoding = c[list(l.keys()).index(letter)]
        coded.append(encoding)
    return "".join(coded)
 
-def decode(es, c):
+def decode(es, c, l):
     def inencodings(code):
         for idx, ci in enumerate(c):
             if ci == code:
                 return idx, True
         return -1, False
-    s = ""
+    s = "" 
     code = ""
-    for letter in es:
+    for letter in es: 
         code += letter
         idx, t = inencodings(code)
         if t:
-            let = list(LETTERS.keys())[idx]
+            let = list(l.keys())[idx]
             s += let
             code = ""
-    return s
-
-def entropy(X):
-    X_hattu = [x/100.0 for x in X]
-    def f(x, y):
-        return x if y == 0 else y * math.log2(1/y) + x
-    return reduce(f, X_hattu, 0)
-
-
-def main():
-    print(USAGE)
-    p = list(LETTERS.values())
-    h = entropy(p)
-    c = encodings(p)
-    if GRAPH:
-        return
-    last = ""
-    while(1):
-        user = input(">").lower()
-        for char in user:
-            if char not in CHARSET:
-                user = user.replace(char, "")
-        luser = user.split(" ")
-        action = luser[0].strip()
-        message = " ".join(luser[1:]).strip()
-        if message == "last":
-            message = last
-        if action == "enc":
-            e    = encode(message, c)
-            last = e
-            print(f"\nENTROPY (H) OF THE CHARSET IS {h}.\nTHE LENGTH (N) OF YOUR MESSAGE (X) IS {len(message)}\nACCORDING TO SHANNON THIS SHOULD BE ENCODABLE TO (almost) N times H bits which is {len(message)*h}")
-            print(f"\nENCODED MESSAGE: {e}\nLENGTH: {len(e)}")
-        elif action == "dec":
-            d    = decode(message, c)
-            last = d
-            print(d)
-        else:
-            print(USAGE)
-        print()
-
-if __name__ == "__main__":
-    main()
+    return s 
---
+++ main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+import sys
+import math
+from functools import reduce
+from huffman import *
+
+ENCCHARSET = " abcdefghijklmnopqrstuvwxyz"
+DECCHARSET = "01"
+USAGE = f"""
+usage:
+     dec <string to decode>
+     enc <string to encode>
+
+Example:
+    >enc kissa istuu puussa
+     10100011001010101010110001001010111011010110101001100111010110101010101010110
+
+    >dec 10100011001010101010110001001010111011010110101001100111010110101010101010110
+     kissa istuu puussa
+
+Please use only items from these charsets:
+
+encoding: {ENCCHARSET} 
+decoding: {DECCHARSET}
+"""
+
+# Letter propabilities from 
+# Lecture 4: 
+#     Entropy and Data Compression (III): Shannon's Source Coding Theorem, Symbol Codes
+#     video: https://youtu.be/eHGqNvkL4n4?t=390
+LETTERS = { 
+    " ":19.28,
+    "a":5.75,	
+    "b":1.28,	
+    "c":2.63,	
+    "d":2.85,	
+    "e":9.13,	
+    "f":1.73,	
+    "g":1.33,	
+    "h":3.13,	
+    "i":5.99,	
+    "j":0.06,	
+    "k":0.84,	
+    "l":3.35,	
+    "m":2.35,	
+    "n":5.96,	
+    "o":6.89,	
+    "p":1.92,	
+    "q":0.08,	
+    "r":5.08,	
+    "s":5.67,	
+    "t":7.06,	
+    "u":3.34,	
+    "v":0.69,	
+    "w":1.19,	
+    "x":0.73,	
+    "y":1.64,	
+    "z":0.07
+}
+
+def entropy(X):
+    def f(x, y):
+        return x if y == 0 else y * math.log2(1/y) + x
+    return reduce(f, X, 0)
+
+def clean(ss, cc):
+    for si in ss:
+        if si not in cc:
+            ss = ss.replace(si, "")
+    return ss
+
+def get_input(last):
+    user    = input(">").lower().split(" ")
+    action  = user[0].strip()
+    message = " ".join(user[1:]).strip()
+    if message == "last":
+        message = last
+    return action, message
+
+def dispatch(action, message, c, h):
+    if action == "enc":
+        m = clean(message, ENCCHARSET)
+        e = encode(m, c, LETTERS)  
+        print(f"ENCODED MESSAGE: {e}\nLENGTH: {len(e)}\nSHANNON OPTIMAL LENGTH: {h*len(m)}") 
+        return e
+    elif action == "dec":
+        m = clean(message, DECCHARSET)
+        d = decode(m, c, LETTERS)
+        print(f"DECODED MESSAGE: {d}\nLENGTH: {len(d)}") 
+        return d
+    else:
+        print(USAGE)
+    return None
+
+def main():
+    p = list(LETTERS.values())
+    h = entropy([pi/100.0 for pi in p])
+    c = build_codewords(p)
+    last = None
+    while(1):
+        action, message = get_input(last) 
+        last = dispatch(action, message, c, h) 
+           
+if __name__ == "__main__":
+    print(USAGE)
+    main()
+
